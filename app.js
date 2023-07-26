ccca03c6a045109c74efd796a19e3e06822c5ffc
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2023-07-25 23:34:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+// app.js
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Importa el módulo cors
 const { databaseService } = require('./databaseService');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/granja",databaseService );
+app.use(cors()); // Usa el middleware CORS
+
+/* require('./routes')(app, databaseService());
+ */
+
 // Middleware de CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -14,7 +20,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-require('./routes')(app, databaseService());
+
+// Monta el middleware databaseService en la ruta base "/granja".
+app.use("/granja", databaseService);
 
 app.listen(3003, () => {
   console.log('App listening on port http://localhost:3003');
